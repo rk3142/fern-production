1497ba13941c6c789de18c3b59b9db790c78361f
Rishav Kumar <rishav.kumar@columbia.edu>
2021-10-29 21:49:41 -0400

    Added base feature template and spec template

---
+++ features/support/paths.rb
@@ -0,0 +1,15 @@
+# TL;DR: YOU SHOULD DELETE THIS FILE
+#
+# This file is used by web_steps.rb, which you should also delete
+#
+# You have been warned
+module NavigationHelpers
+  # Maps a name to a path. Used by the
+  #
+  #   When /^I go to (.+)$/ do |page_name|
+  #
+  # step definition in web_steps.rb
+  #
+end
+
+World(NavigationHelpers)
