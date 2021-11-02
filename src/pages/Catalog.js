9965462c3379e1286e2b43eb2431c10dc1595ee3
AumUpadhyay <aumspeaks@gmail.com>
2021-11-01 20:19:18 -0400

    Added SearchBar, logo, and fixed header size

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,40 +1,62 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { Navigation } from 'react-minimal-side-navigation';
+import { AiOutlineMenu } from "react-icons/ai";
 
+import logo from '../assets/logo.png'
+import fern_text from '../assets/fern_text.png'
+import search_icon from '../assets/search_icon.png'
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
 import './Catalog.css';
 
 export default class Catalog extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      email: "",
-      password: ""
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            cart_count: 0,
+            search: ''
+        };
+    }
 
-  componentDidMount() {
-    var email = localStorage.getItem("email")
-    //alert("email: " + email)
-  }
+    handleLogIn() {
+        this.props.history.push("/productdetails");
+    }
 
-  updateEmail(event) {
-    console.log(event)
-    this.setState({ value: event })
-  }
+    updateSearchQuery = (query) => {
+        this.setState({search: query.target.value})
+    }
 
-  handleLogIn() {
-    this.props.history.push("/productdetails");
-  }
+    submitSearchQuery = () => {
+        this.setState({search: ""})
+    }
 
-  render() {
-    return (
-      <div className = "App">
-        <p className = "Title">Login</p>
-        <button onClick={() => this.handleLogIn()} className = "Button-Primary">Sign In</button>
-        <div className = "Expander"></div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="Header">
+                    <img className="Logo" src={logo} />
+                    <img className="Fern" src={fern_text} />
+                    <div className="SearchBar">
+                        <input className = "SearchBar" 
+                            value={this.state.search} 
+                            onChange={(query) => {this.updateSearchQuery(query)}} 
+                            type="search"
+                        />
+                        <div className = "SearchButton" onClick = {() => this.submitSearchQuery()}>
+                            <img className="SearchIcon" src={search_icon} />
+                        </div>
+                    </div>
+                    
+                    <div className="CartCountContainer">
+                        <div className="CartCount">
+                            <p className="CartCountText">{this.state.cart_count}</p>
+                            <p className="CartCountText2"> IN CART</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
