c49422827c5563e0ba37e962692ac79439b0301a
Rishav Kumar <rishav.kumar@columbia.edu>
2021-11-02 13:58:48 -0400

    Updated code after writing rspec

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-describe ProductsController do
-  describe 'user login', type: :request  do
-    before(:each) {}
-    it 'create use' do
-    end
-    it ''
-    it ''
-  end
-end
+
+
+# describe ProductsController do
+#   describe 'user login', type: :request  do
+#     before(:each) {}
+#     it 'create use' do
+#     end
+#     it ''
+#     it ''
+#   end
+# end
