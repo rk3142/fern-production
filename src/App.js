fcd32b557a88ca49e387afed2464589e9ba2b30a
eureyuri <eureynoguchi@gmail.com>
2021-11-03 12:52:06 -0400

    style: landing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default class App extends Component {
   render() {
     return (
         <>
-          <div id = "EntryText">Feeling Blue? Go GREEN</div>
+          <div className="entry-text">
+              <div className="entry-text__block">Feeling <span className="entry-text__block--blue">Blue</span>?</div>
+              <div className="entry-text__block">Go <span className="entry-text__block--green">GREEN</span></div>
+          </div>
         </>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SearchBar(props) {
     return (
-        <div>A placeholder search bar</div>
+        <div>search bar</div>
     );
 }
 
