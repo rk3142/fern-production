83665817e4dbe378ca8ad86ac754812e412433e0
Rishav Kumar <rishav.kumar@columbia.edu>
2021-11-02 22:02:53 -0400

    Removed unncessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "main#index"
+  #root to: "main#index"
   get '/products', to: 'product#index'
   get '/products/:product_id', to: 'product#show'
   get '/product_type', to: 'product#all_product_types'
