a48fdf1dc1dd326192aeceb8c8ae39b4a18e4b52
Rishav Kumar <rishav.kumar@columbia.edu>
2021-11-03 23:51:54 -0400

    Removed wrong front-end

--- front-end/app/models/product.rb
+++
@@ -1,8 +0,0 @@
-class Product < ApplicationRecord
-  self.table_name = "products"
-  self.primary_key = "product_id"
-
-  def self.find_all_by_product_type product_type
-    Product.where(product_type: product_type)
-  end
-end
--- front-end/app/models/product_type.rb
+++
@@ -1,4 +0,0 @@
-class ProductType < ApplicationRecord
-  self.table_name = "product_types"
-  has_one :product
-end
