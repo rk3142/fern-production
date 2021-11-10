57ab2f6e53f5b85a76eb70d4a5eeef2c2ec1ad44
eureyuri <eureynoguchi@gmail.com>
2021-11-10 10:34:38 -0500

    feat: search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import {Button} from "@mui/material";
 
 function SearchBar(props) {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const submitSearch = event => {
+        event.preventDefault()
+        console.log(searchTerm)
+    }
+
     return (
-        <div className="SearchBar">search bar</div>
+        <Paper
+            component="form"
+            onSubmit={submitSearch}
+            sx={{ p: '2px 6px', display: 'flex', alignItems: 'center', width: 400, borderRadius: '1rem' }}
+        >
+            <InputBase
+                sx={{ ml: 2, flex: 1 }}
+                placeholder="Search"
+                inputProps={{ 'aria-label': 'search fern' }}
+                onChange={e => setSearchTerm(e.target.value)}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                <SearchIcon sx={{color: '#43AC3A'}} />
+            </IconButton>
+        </Paper>
     );
 }
 
