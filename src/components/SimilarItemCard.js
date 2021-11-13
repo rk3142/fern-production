fd20a25c099717a0eab7cb79b7540fc12ee4d0bc
AumUpadhyay <aumspeaks@gmail.com>
2021-11-13 12:29:07 -0500

    Added Similar Items

---
+++ src/components/SimilarItemCard.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { Card, CardActions, CardContent } from "@mui/material";
+import NumberFormat from 'react-number-format';
+import LinesEllipsis from 'react-lines-ellipsis'
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import { LaunchOutlined } from "@mui/icons-material";
+
+import './SimilarItemCard.css'
+import co2_icon from "../assets/CO2.png";
+import h2o_icon from "../assets/h2o_icon.png";
+import energy_icon from "../assets/energy_icon.jpeg";
+import StarRatings from "react-star-ratings";
+import { useHistory } from "react-router-dom";
+
+function SimilarItemCard({ item }) {
+    const [hover, setHover] = useState(false)
+    const [saved, setSaved] = useState(false)
+
+    const { product_name,
+        image_url,
+        link,
+        prices,
+        rating,
+        carbon,
+        water,
+        energy } = item
+
+    let history = useHistory()
+
+    const goToDetails = async () => {
+        localStorage.setItem('recently_clicked', JSON.stringify(item))
+        history.push("/productdetails");
+    }
+
+    return (
+        <Card>
+            <CardContent onClick={goToDetails} className={'card'}>
+                <div className="item__name">{product_name}</div>
+                <div className="item__body">
+                    <div className="item__eco_status">
+                        <div className="EcoStats">
+                            <img className="EcoStatsIcon" src={co2_icon} />
+                            <p className="EcoStatsText">{carbon} kg</p>
+                        </div>
+                        <div className="EcoStats">
+                            <img className="EcoStatsIcon" src={h2o_icon} />
+                            <p className="EcoStatsText">{water}k Liters</p>
+                        </div>
+                        <div className="EcoStats">
+                            <img className="EcoStatsIcon" src={energy_icon} />
+                            <p className="EcoStatsText">{energy} kWh</p>
+                        </div>
+                    </div>
+
+                    <div className="product_image">
+                        <img className="product_image__img" src={image_url} />
+                    </div>
+                </div>
+
+                <div className="product_details">
+                    <div className="product_details__price_seller">
+                        <div className="ProductPrice">${prices[0]["price"]}</div>
+                    </div>
+
+                    <div className="product_details__ratings">
+                        <div className="ProductRatingText">{rating}</div>
+                        <div className="ProductRating">
+                            <StarRatings
+                                rating={rating}
+                                starRatedColor="#FFDC61"
+                                numberOfStars={5}
+                                name='rating'
+                                starDimension="15"
+                                starSpacing="5"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default SimilarItemCard;
--- src/pages/ProductDetails/index.js
+++ src/pages/ProductDetails/index.js
@@ -1,98 +1,117 @@
-import React, { Component } from "react";
+import React, {useEffect} from "react";
 import StarRatings from "react-star-ratings";
 import co2_icon from '../../assets/CO2.png'
 import h2o_icon from '../../assets/h2o_icon.png'
 import energy_icon from '../../assets/energy_icon.jpeg'
 import './ProductDetails.css';
-import {Button} from "@mui/material";
+import SimilarItemCard from "../../components/SimilarItemCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCatalog, selectProducts } from "../../reducers/catalogSlice";
+import { Button, CircularProgress } from "@mui/material";
+import { useState } from "react";
 
 const goToLink = (link) => {
     window.location.href = link
     return null
 }
 
-class ProductDetails extends Component {
+function ProductDetails() { 
+    const dispatch = useDispatch();
+    const [product, setProduct] = useState(JSON.parse(localStorage.getItem('recently_clicked')))
+    const [similarProducts, setSimilarProducts] = useState([])
+    const items = useSelector(selectProducts).filteredProducts
+    const status = useSelector(selectProducts).status
 
-  constructor(props) {
-      super(props);
-      this.state = {
-        product: [],
-      };
-  }
+    useEffect(() => {
+        dispatch(getAllCatalog())
+        setSimilarProducts(getSimilarProducts())
+        console.log(items)
+        console.log(similarProducts)
+    }, [])
 
-  componentWillMount = () => {
-      let product = localStorage.getItem('recently_clicked')
-      this.setState({ product: JSON.parse(product) })
-  }
+    const getSimilarProducts = () => {
+        let similar_items = []
+        for(var i in items) {
+            if(product["product_id"] != items[i]["product_id"] && product["brand"] == items[i]["brand"]) {
+                similar_items.push(items[i])
+            }
+        }
+        return similar_items;
+    }
 
-  render() {
     return (
         <div className='details_page'>
-          <div className="details">
-            <img className="details__image" src={this.state.product['image_url']} />
+            <div className="details">
+                <img className="details__image" src={product['image_url']} />
 
-            <div className="details__info">
-                <h1 className="details__info__name">{this.state.product['product_name']}</h1>
+                <div className="details__info">
+                    <h1 className="details__info__name">{product['product_name']}</h1>
 
-                <div className="details__info__body">
-                    <img className="EcoDetsIcon" src={co2_icon} />
-                    <p className="EcoDetsText">{this.state.product['carbon']}kg of carbon released into the atmosphere</p>
-                    <img className="EcoDetsIcon" src={h2o_icon} />
-                    <p className="EcoDetsText">{this.state.product['water']}k liters of water used in manufacturing</p>
-                    <img className="EcoDetsIcon" src={energy_icon} />
-                    <p className="EcoDetsText">{this.state.product['energy']}kWh of energy used in production</p>
-                </div>
-
-                <div className="details__info__more">
-                    <div className="details__info__more__actions">
-                        <div className="details__info__more__actions__ratings">
-                            <div className="details__info__more__actions__ratings__stars">
-                                {`${this.state.product['rating']}/5.0`}
-                                <StarRatings
-                                  rating={this.state.product['rating']}
-                                  starRatedColor="#FFDC61"
-                                  numberOfStars={5}
-                                  name='rating'
-                                  starDimension="15"
-                                  starSpacing="5"
-                                />
-                            </div>
-
-                            {`${this.state.product['ratings']} ratings `}
-
-                            <div className="details__info__more__actions__ratings__stars__source">
-                                (Amazon)
-                            </div>
-                        </div>
-
-                        <div className="details__info__more__actions__price">
-                            {`$${this.state.product['prices'][0]['price']}`}
-                        </div>
-
-                        <div className="details__info__more__actions__buttons">
-                            <Button variant='contained' className={'details__info__more__actions__buttons-save'}>Save</Button>
-                            <Button variant='contained'
-                                    className={'details__info__more__actions__buttons-buy'}
-                                    onClick={() => goToLink(this.state.product['link'])}
-                            >
-                                Buy
-                            </Button>
-                        </div>
+                    <div className="details__info__body">
+                        <img className="EcoDetsIcon" src={co2_icon} />
+                        <p className="EcoDetsText">{product['carbon']}kg of carbon released into the atmosphere</p>
+                        <img className="EcoDetsIcon" src={h2o_icon} />
+                        <p className="EcoDetsText">{product['water']}k liters of water used in manufacturing</p>
+                        <img className="EcoDetsIcon" src={energy_icon} />
+                        <p className="EcoDetsText">{product['energy']}kWh of energy used in production</p>
                     </div>
 
-                    <div className="details__info__more__description">
-                        {this.state.product['product_description']}
+                    <div className="details__info__more">
+                        <div className="details__info__more__actions">
+                            <div className="details__info__more__actions__ratings">
+                                <div className="details__info__more__actions__ratings__stars">
+                                    {`${product['rating']}/5.0`}
+                                    <StarRatings
+                                        rating={product['rating']}
+                                        starRatedColor="#FFDC61"
+                                        numberOfStars={5}
+                                        name='rating'
+                                        starDimension="15"
+                                        starSpacing="5"
+                                    />
+                                </div>
+
+                                {`${product['ratings']} ratings `}
+
+                                <div className="details__info__more__actions__ratings__stars__source">
+                                    (Amazon)
+                                </div>
+                            </div>
+
+                            <div className="details__info__more__actions__price">
+                                {`$${product['prices'][0]['price']}`}
+                            </div>
+
+                            <div className="details__info__more__actions__buttons">
+                                <Button variant='contained' className={'details__info__more__actions__buttons-save'}>Save</Button>
+                                <Button variant='contained'
+                                    className={'details__info__more__actions__buttons-buy'}
+                                    onClick={() => goToLink(product['link'])}
+                                >
+                                    Buy
+                                </Button>
+                            </div>
+                        </div>
+
+                        <div className="details__info__more__description">
+                            {product['product_description']}
+                        </div>
                     </div>
                 </div>
             </div>
-          </div>
 
-          <div className="similar_products">
-              <div className="similar_products__title">Similar Products</div>
-          </div>
+            <div className="similar_products">
+                <div className="similar_products__title">Similar Products</div>
+                <div className={'catalog__list'}>
+                {
+                    status === 'success' ?
+                        similarProducts.map(item => <SimilarItemCard item={item} key={item.product_id} />)
+                        : <CircularProgress />
+                }
+            </div>
+            </div>
         </div>
     );
-  }
 }
 
 export default ProductDetails
