ae2ee17174ec688e769419fb55a2e3f644d46302
eureyuri <eureynoguchi@gmail.com>
2021-12-17 22:36:08 -0500

    test: coverage for product details

--- src/pages/ProductDetails/index.test.js
+++ src/pages/ProductDetails/index.test.js
@@ -5,14 +5,18 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { render, fireEvent, screen } from '../../common/test-utils'
 import {BASE_URL} from "../../api";
+import {MemoryRouter} from "react-router-dom";
 
 export const handlers = [
-  rest.get(BASE_URL + '/products', (req, res, ctx) => {
-    return res(ctx.json({products: apiMock()}), ctx.delay(100))
-  }),
-  rest.get(BASE_URL + '/products/' + apiMock()[0], (req, res, ctx) => {
-    return res(ctx.json(apiMock()[0]), ctx.delay(100))
-  })
+    rest.get(BASE_URL + '/products', (req, res, ctx) => {
+        return res(ctx.json({products: apiMock()}), ctx.delay(100))
+    }),
+    rest.get(BASE_URL + '/products/' + apiMock()[0], (req, res, ctx) => {
+        return res(ctx.json(apiMock()[0]), ctx.delay(100))
+    }),
+    rest.get(BASE_URL + '/product/similar', (req, res, ctx) => {
+        return res(ctx.json(apiMock()[0]), ctx.delay(100))
+    })
 ]
 
 const server = setupServer(...handlers)
@@ -26,14 +30,22 @@ afterEach(() => server.resetHandlers())
 describe('Product details', () => {
     it('renders', async () => {
         localStorage.setItem('recently_clicked', JSON.stringify(apiMock()[0]))
-        render(<ProductDetails />)
+        render(
+            <MemoryRouter>
+                <ProductDetails />
+            </MemoryRouter>
+        )
         const itemTitle = await screen.findByText(/Generic/i)
         expect(itemTitle).toBeInTheDocument()
     });
 
     it('saves item', async () => {
         localStorage.setItem('recently_clicked', JSON.stringify(apiMock()[0]))
-        const {container} = render(<ProductDetails />)
+        const {container} = render(
+            <MemoryRouter>
+                <ProductDetails />
+            </MemoryRouter>
+        )
         const saveBtn = container.firstChild.getElementsByClassName('details__info__more__actions__buttons-save')[0]
         fireEvent.click(saveBtn)
         const unsaveBtn = await screen.findByText(/Unsave/i)
@@ -42,7 +54,11 @@ describe('Product details', () => {
 
     it('unsaves item', async () => {
         localStorage.setItem('recently_clicked', JSON.stringify(apiMock()[0]))
-        const {container} = render(<ProductDetails />)
+        const {container} = render(
+            <MemoryRouter>
+                <ProductDetails />
+            </MemoryRouter>
+        )
         const saveBtn = container.firstChild.getElementsByClassName('details__info__more__actions__buttons-save')[0]
         fireEvent.click(saveBtn)
 
@@ -61,7 +77,11 @@ describe('Product details', () => {
         });
 
         localStorage.setItem('recently_clicked', JSON.stringify(apiMock()[0]))
-        const {container} = render(<ProductDetails />)
+        const {container} = render(
+            <MemoryRouter>
+                <ProductDetails />
+            </MemoryRouter>
+        )
         const buyBtn = container.firstChild.getElementsByClassName('details__info__more__actions__buttons-buy')[0]
         fireEvent.click(buyBtn)
 
@@ -71,7 +91,11 @@ describe('Product details', () => {
     it('get similar products', async () => {
         // FIXME: wait until all similar products load
         localStorage.setItem('recently_clicked', JSON.stringify(apiMock()[0]))
-        const {container} = await render(<ProductDetails />)
+        const {container} = await render(
+            <MemoryRouter>
+                <ProductDetails />
+            </MemoryRouter>
+        )
         const myElement = await screen.findAllByText(/Generic/i)
 
         expect(myElement.length).toBe(1)
